9501d79dc87f83754714c752baecc556eab7244c
vi_ser <vinceulisse@gmail.com>
2024-04-19 14:01:29 +0200

    aggiunto show (view e route)

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -47,7 +47,8 @@ class ProjectController extends Controller
      */
     public function show(Project $project)
     {
-        //
+        return view('admin.projects.show', compact('project'));
+
     }
 
     /**
